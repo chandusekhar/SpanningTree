24c86381f361522fefbfaa09ce8b5eac49af3fbc
Marat Turaev <marat.turaev@hotmail.com>
2012-11-05 23:42:19 +0400

    Kruskal draft

---
+++ Domain/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain {
+    public class Graph {
+        public List<Vertex<int>> Vertexes { get; set; }
+        public List<Edge<int>> Edges { get; set; }
+    }
+
+    public class Vertex<T> {
+        public T Id { get; set; }
+    }
+
+    public class Edge<T> {
+        public int Weight { get; set; }
+        public Vertex<T> From { get; set; }
+        public Vertex<T> To { get; set; }
+    }
+
+    public class WeightComparer<T> : IComparer<Edge<T>> {
+        public int Compare(Edge<T> x, Edge<T> y) {
+            return x.Weight.CompareTo(y.Weight);
+        }
+    }
+}
